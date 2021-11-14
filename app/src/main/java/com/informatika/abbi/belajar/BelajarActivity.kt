76b8dbf51b1046899ba7c481eda6159b43c1feca
DESKTOP-FV08FA3\Rima <danapradana11@gmail.com>
2021-11-14 20:26:18 +0700

    item_list prepared for recyclerview grod

---
+++ app/src/main/java/com/informatika/abbi/belajar/BelajarActivity.kt
@@ -0,0 +1,12 @@
+package com.informatika.abbi.belajar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.informatika.abbi.R
+
+class BelajarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_belajar)
+    }
+}
