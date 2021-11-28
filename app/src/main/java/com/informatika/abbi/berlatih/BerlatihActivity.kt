f71a643d4c4fa0205c53f67874c665f98f23ec29
DESKTOP-FV08FA3\Rima <danapradana11@gmail.com>
2021-11-28 11:58:44 +0700

    modify activity_berlatih.xml

--- app/src/main/java/com/informatika/abbi/berlatih/BerlatihActivity.kt
+++ app/src/main/java/com/informatika/abbi/berlatih/BerlatihActivity.kt
@@ -4,10 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.bumptech.glide.Glide
-import com.google.firebase.firestore.FirebaseFirestore
-import com.informatika.abbi.data.BelajarData
 import com.informatika.abbi.data.BerlatihAsetData.soal
 import com.informatika.abbi.data.BerlatihData
 import com.informatika.abbi.databinding.ActivityBerlatihBinding
@@ -20,10 +19,6 @@ class BerlatihActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityBerlatihBinding
 
-    private val db = FirebaseFirestore.getInstance()
-    private val collection = db.collection("questions")
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBerlatihBinding.inflate(layoutInflater)
@@ -31,58 +26,6 @@ class BerlatihActivity : AppCompatActivity(){
 
         getQuestions()
 
-        /*collection.limit(1).get()
-            .addOnSuccessListener { result ->
-                for(document in result){
-                    val images = document.getString("question")
-                    val options = document.get("options") as ArrayList<String>
-                    val answer = document.getString("answer")
-                    val optionA = options[0]
-                    val optionB = options[1]
-                    val optionC = options[2]
-                    val optionD = options[3]
-                    val image = soal.getValue(key = "$images")
-                    Glide.with(this)
-                        .load(image)
-                        .into(binding.imgQuestion)
-                    binding.btnAnswerA.text = optionA
-                    binding.btnAnswerB.text = optionB
-                    binding.btnAnswerC.text = optionC
-                    binding.btnAnswerD.text = optionD
-
-                    binding.btnAnswerA.setOnClickListener{
-                        Log.d(TAG, "onCreate: $optionA == $answer")
-                        if(optionA == answer) {
-                            binding.btnAnswerA.text = "Benar"
-                        }else{
-                            binding.btnAnswerA.text = "Salah"
-                        }
-                    }
-                    binding.btnAnswerB.setOnClickListener{
-                        if(optionB == answer)
-                            binding.btnAnswerB.text = "Benar"
-                        else
-                            binding.btnAnswerB.text = "Salah"
-                    }
-                    binding.btnAnswerC.setOnClickListener{
-                        if(binding.btnAnswerC.text == answer)
-                            binding.btnAnswerC.setBackgroundColor(Color.GREEN)
-                        else
-                            binding.btnAnswerC.setBackgroundColor(Color.RED)
-                    }
-                    binding.btnAnswerD.setOnClickListener{
-                        if(binding.btnAnswerD.text == answer)
-                            binding.btnAnswerD.setBackgroundColor(Color.GREEN)
-                        else
-                            binding.btnAnswerD.setBackgroundColor(Color.RED)
-                    }
-
-                }
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Exception",e )
-            }
-*/
         binding.btnNext.setOnClickListener{
             finish();
             startActivity(Intent(this, this@BerlatihActivity::class.java));
@@ -90,6 +33,7 @@ class BerlatihActivity : AppCompatActivity(){
     }
 
     private fun getQuestions(){
+        binding.pbBerlatih.visibility = View.VISIBLE
         val client = AsyncHttpClient()
         val url = "https://abbi-api.herokuapp.com/api/questions"
         client.get(url, object: AsyncHttpResponseHandler(){
@@ -118,14 +62,16 @@ class BerlatihActivity : AppCompatActivity(){
                         Log.d("This", "onSuccess: $listQuestions")
                     }
 
-                    val random = (-1 until listQuestions.size).random()
+                    val random = (0 until listQuestions.size).random()
                     val getRandomList = listQuestions[random]
                     val imageData = soal.getValue("${getRandomList.images}")
+                    binding.pbBerlatih.visibility = View.GONE
                     Glide.with(this@BerlatihActivity).load(imageData).into(binding.imgQuestion)
                     binding.btnAnswerA.text = getRandomList.options?.get(0).toString()
                     binding.btnAnswerB.text = getRandomList.options?.get(1).toString()
                     binding.btnAnswerC.text = getRandomList.options?.get(2).toString()
                     binding.btnAnswerD.text = getRandomList.options?.get(3).toString()
+                    binding.tvQuestion.visibility = View.VISIBLE
                     Log.d("This", "onSuccess: $listQuestions, random = $random")
 
                     binding.btnAnswerA.setOnClickListener{
@@ -164,7 +110,7 @@ class BerlatihActivity : AppCompatActivity(){
             }
 
             override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
-                TODO("Not yet implemented")
+                Toast.makeText(this@BerlatihActivity, "${error.toString()} status: $statusCode", Toast.LENGTH_SHORT).show()
             }
 
         })
